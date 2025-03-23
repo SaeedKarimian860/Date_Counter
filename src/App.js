942f06ad6881fdd8252dff7807c1dfbcb27f7ba8
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-22 21:58:14 -0400

    Date Counter

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
   const date = new Date();
   date.setDate(date.getDate() + count);
@@ -17,17 +18,23 @@ function Counter() {
   return (
     <div>
       <div>
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
         <span>Count: {count}</span>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
         <span>
           {count === 0
-            ? "Today is"
+            ? "Today is "
             : count > 0
-            ? `${count} days from today is`
-            : `${count} days ago was`}
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
         </span>
         <span>{date.toDateString()}</span>
       </p>

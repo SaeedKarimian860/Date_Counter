434dd693ed7fa5781b020ec6d5e8f75c842188c9
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-22 21:48:46 -0400

    Date Counter

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ export default function App() {
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
   return (
     <div>
       <div>
@@ -18,6 +21,16 @@ function Counter() {
         <span>Count: {count}</span>
         <button onClick={() => setCount((c) => c + 1)}>+</button>
       </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is"
+            : count > 0
+            ? `${count} days from today is`
+            : `${count} days ago was`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
